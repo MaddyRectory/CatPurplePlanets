d6f8ce924889945064a71c789ad06a3e624dc04d
Mailys RIVIERE <mailys.riviere@gmail.com>
2025-11-11 18:21:00 +0100

    add notes in main pour prevoir les tests

---
+++ 5_/ex02/main.cpp
@@ -0,0 +1,31 @@
+/*
+creation announce destruction (check form permission)
+Shrub
+Rbo
+pard
+
+
+
+TODO: check char inerdits dans noms de fichier asef efasdasfsdfas
+Si oui check target > ex target invalid
+
+creation shrubbery form
+cas ok
+shruberry allready planted
+si ficher existe deja, planter a la place ou apres ?
+
+Robotomy
+faire 10 form, les faire signer par un buro
++> check le random
+Ex : robo fail
+TODO : try/catch a faire au lvl superieur comme le buro
+
+Pardon
+just print the info
+
+executor
+ex : grade too low
+ex : form pas signe
+
+
+*/
