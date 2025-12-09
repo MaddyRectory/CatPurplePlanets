5dcc5b37f3c30c8a4cc91b674f8ac013f93c1830
Mailys RIVIERE <mailys.riviere@gmail.com>
2025-12-09 18:16:03 +0100

    fix main after tests

--- 5_/ex00/main.cpp
+++ 5_/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mairivie <mairivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 11:20:04 by mairivie          #+#    #+#             */
-/*   Updated: 2025/12/09 15:01:13 by mairivie         ###   ########.fr       */
+/*   Updated: 2025/12/09 18:15:31 by mairivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,8 @@ int main(void){
 std:: cout << "\n ------ Test 1 - constructors/destructors ------ \n" << std::endl;
     try
     {
-    std::string coucou = NULL;
-     
     Bureaucrat def;
     Bureaucrat Ok(coucou, 42);
-    
 
     std::cout << CYAN << def << RESET;
     std::cout << CYAN << Ok << RESET;
