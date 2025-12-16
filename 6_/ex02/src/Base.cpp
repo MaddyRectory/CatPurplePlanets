bfd605adaad4e926462a930a161848711723fc8e
Mailys RIVIERE <mailys.riviere@gmail.com>
2025-12-16 16:08:31 +0100

    compilation ok, random ok, multi instance ptr ok

--- 6_/ex02/src/Base.cpp
+++ 6_/ex02/src/Base.cpp
@@ -6,42 +6,13 @@
 /*   By: mairivie <mairivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 14:29:30 by mairivie          #+#    #+#             */
-/*   Updated: 2025/12/16 16:00:57 by mairivie         ###   ########.fr       */
+/*   Updated: 2025/12/16 16:04:49 by mairivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Base.hpp"
-#include "../include/A.hpp"
-#include "../include/B.hpp"
-#include "../include/C.hpp"
-#include "../include/colors.hpp"
-
-#include <iostream> // std::cout
-#include <ctime> // time 
-#include <cstdlib> // rand()
 
 Base::~Base() { }
 
-// randomly instantiates A, B, or C and returns the instance as a Base pointer.
-// Use anything you like for the random choice implementation.
-// std::srand((unsigned int)std::time(0)); // < init random en debut de main, pas a chaque ft
-// Base * generate(void) {
-//     int alea = rand() % 3; // < /r 1d3
-//     Base * base = NULL;
-//     switch (alea) {
-//         case 0 :
-//             base = new A;
-//             break;
-//         case 1 :
-//             base = new B;
-//             break;
-//         case 2 :
-//             base = new C;
-//             break;                
-//         default :
-//             std::cout << "Unknokn form\n";
-//         }
-//     return base;
-// }
 
 
---
+++ 6_/ex02/src/main.cpp
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mairivie <mairivie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/16 14:07:00 by mairivie          #+#    #+#             */
+/*   Updated: 2025/12/16 16:01:33 by mairivie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include "../include/colors.hpp"
+#include "../include/Base.hpp"
+#include "../include/A.hpp"
+#include "../include/B.hpp"
+#include "../include/C.hpp"
+
+// randomly instantiates A, B, or C and returns the instance as a Base pointer.
+// Use anything you like for the random choice implementation.
+Base * generate(void) {
+    int alea = rand() % 3; // < /r 1d3
+    Base * ptr = NULL;
+    switch (alea) {
+        case 0 :
+            ptr = new A;
+            break;
+        case 1 :
+            ptr = new B;
+            break;
+        case 2 :
+            ptr = new C;
+            break;                
+        default :
+            std::cout << "Unknokn form\n";
+        }
+    return ptr;
+}
+
+// Prints the actual type of the object pointed to by p: "A", "B", or "C".
+void identify(Base* p) {
+        if (dynamic_cast<A*>(p))
+            std::cout << "C'est un A\n";
+        if (dynamic_cast<B*>(p))
+            std::cout << "C'est un B\n";
+        if (dynamic_cast<C*>(p))
+            std::cout << "C'est un C\n";
+    
+}
+
+// // Prints the actual type of the object referenced by p: "A", "B", or "C". 
+// // Using a pointer inside this function is forbidden.
+// void identify(Base& p) {
+    
+// }
+
+int main (void) {
+    
+    std::cout << GREEN "\nCompilation OK !" RESET << std::endl;
+    
+    std::srand((unsigned int)std::time(0)); // < init random en debut de main, pas a chaque ft
+    
+    std::cout << BG_BLUE "\nTest random generator" RESET << std::endl;
+        int table[6];
+        for (int i = 0; i <= 5; i++) { 
+            table[i] = rand() % 3;
+            std::cout << "Case " << i << " is " << table[i] << " \n"; 
+        }
+
+    std::cout << BG_BLUE "\nGenerate & Identify one instance (ptr)" RESET << std::endl;
+        Base * ptr = NULL; 
+        ptr = generate();
+        identify(ptr);
+        delete ptr;
+
+    std::cout << BG_BLUE "\nGenerate & Identify multi instance (ptr)" RESET << std::endl;
+        Base * list[6];
+        for (int i = 0; i <= 5; i++) { 
+            list[i] = generate();
+            identify(list[i]);
+            delete list[i];
+        }
+    
+        return 0;
+}
