59c2804504b37c8c97f2bc1b7d4fcc5344067fde
Mailys RIVIERE <mailys.riviere@gmail.com>
2025-12-17 15:29:47 +0100

    initial stage, compil ok

---
+++ 6_/ex01/src/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mairivie <mairivie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/17 15:25:15 by mairivie          #+#    #+#             */
+/*   Updated: 2025/12/17 15:27:02 by mairivie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream> // cin cout tu coco
+#include "../include/colors.hpp"
+#include "../include/Serializer.hpp"
+#include "../include/data.h"
+
+int main (void) {
+    
+    std::cout << GREEN "\nCompilation OK !" RESET << std::endl;
+    
+    return 0;
+}
