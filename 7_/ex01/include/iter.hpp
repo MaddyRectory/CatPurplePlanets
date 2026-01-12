db441da5c9b390a5d8ac9b78059cd3ebff903a59
Mailys RIVIERE <mailys.riviere@gmail.com>
2026-01-12 17:02:16 +0100

    more tests in main, tests ok

--- 7_/ex01/include/iter.hpp
+++ 7_/ex01/include/iter.hpp
@@ -6,22 +6,22 @@
 /*   By: mairivie <mairivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 14:07:10 by mairivie          #+#    #+#             */
-/*   Updated: 2026/01/12 15:45:54 by mairivie         ###   ########.fr       */
+/*   Updated: 2026/01/12 17:01:37 by mairivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 
 template < typename T >
-void    iter(T * array, const int size_array, void function(T & arg) ){
-    for (int i = 0; i <= size_array; i++) {
+void    iter(T * array, int size_array, void function(T & arg) ){
+    for (int i = 0; i < size_array; i++) {
         function(array[i]);
     }
 }
 
 template < typename T >
-void    iter(T * array, const int size_array, void function(const T & arg) ){
+void    iter(T * array, int size_array, void function(const T & arg) ){
         for (int i = 0; i < size_array; i++) {
-        function(array[i]);
-    }
+            function(array[i]);
+        }
 }
--- 7_/ex01/src/main.cpp
+++ 7_/ex01/src/main.cpp
@@ -6,28 +6,48 @@
 /*   By: mairivie <mairivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 14:07:15 by mairivie          #+#    #+#             */
-/*   Updated: 2026/01/12 15:44:10 by mairivie         ###   ########.fr       */
+/*   Updated: 2026/01/12 17:01:08 by mairivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/iter.hpp"
+#include "../include/colors.hpp"
 #include <iostream>
 
 
 template <typename T>
-void print_this_type(T const &x)
+void printType(T const &x)
 {
 	std::cout << x << std::endl;
 }
 
-int main(void) {
-	int intArray[] = {1, 2, 3, 4, 5};
-	std::string stringArray[] = {"Hello", "World", "!"};
-
-	std::cout << "intArray: " << std::endl;
-	iter(intArray, 5, print_this_type<int>);
-
-	std::cout << std::endl << "stringArray: " << std::endl;
-	iter(stringArray, 3, print_this_type<std::string>);
+template <typename T>
+void increase(T &x) {
+    x++;
 }
 
+int main(void) {
+	int             intArray[] = {0, 1, 2, 3, 4};
+    const int       constArray[] = {10, 11, 12, 13, 14}; 
+	std::string     stringArray[] = {"Hello", "World", "!"};
+
+    std::cout << BG_BLUE "\n--- Array of int (modifiable) ---" RESET << std::endl;
+    
+	std::cout << "Initial state:" << std::endl;
+	iter(intArray, 5, printType<int>);
+	iter(intArray, 5, increase<int>);
+    std::cout << "\nAfter increase iteration :" << std::endl;
+    iter(intArray, 5, printType<int>);
+
+    std::cout << BG_BLUE "\n--- Array of int (const) ---" RESET << std::endl;
+    
+    iter(constArray, 5, printType<const int>);
+	// iter(constArray, 5, increase<int>); // Warning ne compilera pas
+    // iter(constArray, 5, printType<int>);
+    
+    std::cout << BG_BLUE "\n--- Array of string ---" RESET << std::endl;
+
+	iter(stringArray, 3, printType<std::string>);
+    // iter(constArray, 5, increase<char>); // Warning ne compilera pas
+    // iter(constArray, 5, printType<int>);
+}
