ad2116a20026158644a1e7cae9213c90d8fc5f92
Mailys RIVIERE <mailys.riviere@gmail.com>
2026-01-12 14:09:57 +0100

    setup requirement, empty files

---
+++ 7_/ex00/src/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mairivie <mairivie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/12 14:05:09 by mairivie          #+#    #+#             */
+/*   Updated: 2026/01/12 14:06:52 by mairivie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/whatever.hpp"
+#include <iostream>
+
+int main( void ) {
+    int a = 2;
+    int b = 3;
+    
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return 0;
+}
