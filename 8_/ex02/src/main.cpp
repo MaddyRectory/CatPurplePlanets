184be3451e33a519142dd80e868b790563cc2da8
M. RIVIERE <123306167+MaddyRectory@users.noreply.github.com>
2026-02-23 14:25:21 +0000

    add MutantStack.hpp main.cpp

---
+++ 8_/ex02/src/main.cpp
@@ -0,0 +1,39 @@
+#include "MutantStack.hpp"
+#include <iostream>
+
+#define PRINT(x) std::cout << x << std::endl
+
+int main()
+{
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+
+	PRINT("Top: ");
+	PRINT(mstack.top());
+
+	mstack.pop();
+
+	PRINT("Size: ");
+	PRINT(mstack.size());
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+	PRINT("Iterator: ");
+	while (it != ite)
+	{
+		PRINT(*it);
+		++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
+}
