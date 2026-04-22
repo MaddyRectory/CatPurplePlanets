f89e1e9dcb25482eaee41421567536ad3bfd0da4
M. RIVIERE <123306167+MaddyRectory@users.noreply.github.com>
2026-04-22 10:28:21 +0000

    starting point : blank files and main

---
+++ 9_/ex00/src/btc.cpp
@@ -0,0 +1,16 @@
+#include "../include/btc.hpp"
+
+int main(int ac, char** av)
+{
+    if (ac != 2)
+    {
+        std::cout << "Error : bad argument\n";
+        return EXIT_FAILURE ;
+    }
+    std::cout << "File name: " << av[1] << std::endl;
+    if ()
+    
+    
+
+    return EXIT_SUCCESS;
+}
