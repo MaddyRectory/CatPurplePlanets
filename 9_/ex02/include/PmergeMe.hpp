e969c53e7b9da34b1c389cffd8e0d9d4883e1427
M. RIVIERE <123306167+MaddyRectory@users.noreply.github.com>
2026-05-15 07:41:54 +0000

    reset hart to 42cluster version

---
+++ 9_/ex02/include/PmergeMe.hpp
@@ -0,0 +1,15 @@
+#define ORANGE "\033[38;5;208m"
+#define RED "\033[38;5;196m"
+#define TEAL "\033[38;5;49m"
+#define PURPLE "\033[38;5;129m"
+#define OLIVE "\033[38;5;58m"
+#define RESET "\033[0m"
+
+#include <map>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <sstream>
+#include <typeinfo>
+#include <cfloat>
---
+++ 9_/ex02/src/main.cpp
@@ -0,0 +1,18 @@
+/*
+void	printNumbers() {
+	std::cout << "Stored values: ";
+	for (std::set<int>::iterator it = _storage.begin(); it != _storage.end(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;	
+}*/
+
+#include "../include/PmergeMe.hpp"
+
+int main(int ac, char ** av) {
+    std::cout << OLIVE "PROGRAM STARTED" RESET << std::endl;
+
+    (void)ac;
+    (void)av;
+
+    return 0;
+}
